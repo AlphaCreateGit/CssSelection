ae4c053b97848da7c2154bb7b92796774e76213b
hai <hai.alphacreative@gmail.com>
2024-07-03 10:19:07 +0700

    update

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
   gsapGui();
   gsapGallery();
   gsapWink();
+  splitText();
   ScrollTrigger.refresh();
 });
 
@@ -269,3 +270,24 @@ var swiper = new Swiper(".mySwiper", {
     rotate: false,
   },
 });
+
+function splitText() {
+  gsap.config({ trialWarn: false });
+  console.clear();
+  gsap.registerPlugin(ScrollTrigger, SplitText);
+  const split = new SplitText("p", { type: "lines" });
+
+  split.lines.forEach((target) => {
+    gsap.to(target, {
+      backgroundPositionX: 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: target,
+        markers: true,
+        scrub: 1,
+        start: "top center",
+        end: "bottom center",
+      },
+    });
+  });
+}
