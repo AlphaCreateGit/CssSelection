13286773cefa04b7597686dd8ed85a2d242d5d07
hai <hai.alphacreative@gmail.com>
2024-07-10 14:17:31 +0700

    add lib lightpick calender

--- script.js
+++ script.js
@@ -588,3 +588,15 @@ function someText() {
     },
   });
 }
+
+// var picker = new Lightpick({
+//   field: document.getElementById("datepicker"),
+//   singleDate: false,
+//   selectForward: true,
+//   // onSelect: function (start, end) {
+//   //   var str = "";
+//   //   str += start ? start.format("Do MMMM YYYY") + " to " : "";
+//   //   str += end ? end.format("Do MMMM YYYY") : "...";
+//   //   // document.getElementById("result-date").innerHTML = str;
+//   // },
+// });
